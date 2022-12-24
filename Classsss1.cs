e9b24b1ef1739f55bbe8403919d70d67d9fc27a2
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-25 01:31:11 +0400

    Добавьте файлы проекта.

---
+++ Classsss1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Classsss1
+    {
+
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; } = 9;
+
+        private int _filds;
+
+        public int p_write
+        {
+            set { _filds = value; }
+        }
+        public int p
+        {
+
+            get { return _filds; }
+            set
+            {
+
+                if (value > 0)
+                {
+                    _filds = value;
+                }
+                else
+                {
+
+                    throw new NullReferenceException("Name is Empty");
+                }
+            }
+        }
+        public Classsss1()
+        {
+
+        }
+        public Classsss1(int a, int b, int c)
+        {
+            MyProperty = a;
+            MyProperty2 = b;
+            _filds = c;
+
+            Console.WriteLine("Arjeqner  " + a + " " + b + " " + c);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classsss1 obj1 = new Classsss1();
+            Console.WriteLine(obj1.MyProperty2);
+
+            Classsss1 obj2 = new Classsss1(1, 2, 3);
+
+            //obj2.p_write = 111;
+            obj1.p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(obj1.p);
+            Console.ReadKey();
+
+        }
+    }
+}
